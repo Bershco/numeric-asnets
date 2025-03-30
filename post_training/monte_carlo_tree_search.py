915711737c2dfc640bfd54dff0618776d0efa777
Bershco <hersco@post.bgu.ac.il>
2025-03-30 18:07:39 +0300

    Changed 'self.children' from a dict of Node:list(Node) to a dict of Node:list(action,Node)

--- post_training/monte_carlo_tree_search.py
+++ post_training/monte_carlo_tree_search.py
@@ -15,23 +15,8 @@ class MCTS:
     def __init__(self, exploration_weight=1):
         self.Q = defaultdict(int)  # total reward of each node
         self.N = defaultdict(int)  # total visit count for each node
-        self.children = dict()  # children of each node
+        self.children = dict()  # actions and children output of each node. structure is (action,result_state)
         self.exploration_weight = exploration_weight
-
-    def choose(self, node):
-        "Choose the best successor of node. (Choose a move in the game)"
-        if node.is_terminal():
-            raise RuntimeError(f"choose called on terminal node {node}")
-
-        if node not in self.children:
-            return node.find_random_child()
-
-        def score(n):
-            if self.N[n] == 0:
-                return float("-inf")  # avoid unseen moves
-            return self.Q[n] / self.N[n]  # average reward
-
-        return max(self.children[node], key=score)
 
     def do_rollout(self, node):
         "Make the tree one layer better. (Train for one iteration.)"
@@ -49,7 +34,7 @@ class MCTS:
             if node not in self.children or not self.children[node]:
                 # node is either unexplored or terminal
                 return path
-            unexplored = self.children[node] - self.children.keys()
+            unexplored = self.children[node][1] - self.children.keys()
             if unexplored:
                 n = unexplored.pop()
                 path.append(n)
@@ -83,7 +68,7 @@ class MCTS:
         "Select a child of node, balancing exploration & exploitation"
 
         # All children of node should already be expanded:
-        assert all(n in self.children for n in self.children[node])
+        assert all(n in self.children for n in self.children[node][1])
 
         log_N_vertex = math.log(self.N[node])
 
@@ -93,7 +78,7 @@ class MCTS:
                 log_N_vertex / self.N[n]
             )
 
-        return max(self.children[node], key=uct)
+        return max(self.children[node][1], key=uct)
 
 
 class Node(ABC):
