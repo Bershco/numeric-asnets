14194cfcc28d4a11e5487715573c15da9fcf8234
Bershco <hersco@post.bgu.ac.il>
2025-05-18 16:38:18 +0300

    Yet another small but meaningful bug

--- asnets/asnets/scripts/run_asnets.py
+++ asnets/asnets/scripts/run_asnets.py
@@ -236,7 +236,8 @@ class MonteCarloPolicyEvaluator(MCTS):
         if self.path_until_goal is not None:
             next_action, next_mcts_node = self.path_until_goal[0]
             self.path_until_goal = self.path_until_goal[1:]
-            self.children[cstate][next_action] = next_mcts_node
+            self.children[self.state_to_node[cstate]][next_action] = next_mcts_node
+            self.state_to_node[next_mcts_node.state] = next_mcts_node 
             return next_action
 
         def score(pair):
