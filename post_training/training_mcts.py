b2876b75ee64c41e200c0a3b8ab6a76911a55b69
Bershco <hersco@post.bgu.ac.il>
2025-10-21 14:07:00 +0300

    fixed training bugs

--- post_training/training_mcts.py
+++ post_training/training_mcts.py
@@ -2,12 +2,13 @@ import numpy as np
 import tensorflow as tf
 
 from asnets.multiprob import to_local
-from .monte_carlo_tree_search import MCTS, wrapInMCTSNode, FixedChildMap
+from .monte_carlo_tree_search import MCTS, wrapInMCTSNode, FixedChildMap, MCTSNode
 
 
 class TrainingMCTS(MCTS):
     """Slim MCTS for training with policy+value network.
     Uses value head instead of rollouts, supports tree re-rooting.
+    Used inside rpyc.Service rather than controlling it.
     """
 
     def __init__(self, network, problem_service,
@@ -17,10 +18,8 @@ class TrainingMCTS(MCTS):
         self.problem_service = problem_service
         self.iterations = iterations
         self.k = expansion_k
-        self.state_to_node = {}
-        self.act_dist_per_node = {}
 
-    def _expand(self, node):
+    def _expand(self, node: MCTSNode):
         """Expand children using policy priors (top-k by prob)."""
         # if node in self.children:
         if node.children is not None:
@@ -30,31 +29,55 @@ class TrainingMCTS(MCTS):
         act_dist = self.get_act_dist_from_mcts_node(node)
         act_dist = to_local(act_dist)
         act_dist = tf.squeeze(act_dist).numpy()
-        self.act_dist_per_node[node] = act_dist
 
         # mask invalid actions
-        mask = [node.is_applicable_action(i) for i in range(len(act_dist))]
+        # mask = [node.is_applicable_action(i) for i in range(len(act_dist))]
+        mask = self.get_applicable_action_mask(node)
         sorted_indices = sorted(range(len(act_dist)), key=lambda i: act_dist[i], reverse=True)
 
         keys, values = [], []
-        selected = 0
+        # selected = 0
+        # for i in sorted_indices:
+        #     if selected >= self.k:
+        #         break
+        #     if not mask[i] or act_dist[i] == 0.0:
+        #         continue
+        #     next_state, step_cost = node.simulate_step(i, self.problem_service)
+        #     child = wrapInMCTSNode(
+        #         next_state,
+        #         cost_until_now=node.cost_until_now + step_cost,
+        #         previous_action=i
+        #     )
+        #     keys.append(i)
+        #     values.append(child)
+        #     self.state_id_to_node[child.state_id] = child
+        #     selected += 1
+        selected_actions = []
         for i in sorted_indices:
-            if selected >= self.k:
+            if len(selected_actions) >= self.k:
                 break
             if not mask[i] or act_dist[i] == 0.0:
                 continue
-            next_state, step_cost = node.simulate_step(i, self.problem_service)
-            child = wrapInMCTSNode(
-                next_state,
-                cost_until_now=node.cost_until_now + step_cost,
-                previous_action=i
-            )
-            keys.append(i)
-            values.append(child)
-            self.state_to_node[child.state] = child
-            selected += 1
+            selected_actions.append(i)
 
-        # self.children[node] = dict(zip(keys, values)) #TODO: change to FixedChildMap
+        results = self.problem_service.exposed_env_simulate_batch_steps(node.state_id, hash(node), selected_actions)
+
+        for (action_id, cstate_after_action_i_id, cstate_after_action_i_hash,
+             step_cost, is_goal, is_terminal,
+             network_ready_repr, applicable_action_mask
+             ) in results:
+            wrapped_output_cstate = wrapInMCTSNode(
+                cstate_id=cstate_after_action_i_id,
+                cost_until_now=node.cost_until_now + step_cost,
+                previous_action=action_id,
+                is_goal=is_goal,
+                is_terminal=is_terminal,
+                as_network_input=network_ready_repr, applicable_action_mask=applicable_action_mask,
+                hashed_state=cstate_after_action_i_hash,
+            )
+            keys.append(action_id)
+            values.append(wrapped_output_cstate)
+
         node.children = FixedChildMap(keys,values)
 
     def _evaluate_node(self, node) -> float:
@@ -62,18 +85,34 @@ class TrainingMCTS(MCTS):
 
     def _rollout(self, node, horizon=0):
         """Use value head for evaluation instead of random rollout."""
-        value = self.get_value_from_mcts_node(node)
-        value = to_local(value)
-        return float(tf.squeeze(value).numpy())
+        return self.get_value_from_mcts_node(node)
 
     def initialise_tree(self, cstate):
         """Start a new tree for a fresh episode."""
-        self.curr_tree_root = wrapInMCTSNode(cstate, cost_until_now=0, previous_action=None)
-        self.state_to_node[self.curr_tree_root.state] = self.curr_tree_root
+        cstate_id, cstate_hash = self.problem_service.internal_get_state_identifiers(cstate)
+        self.curr_tree_root = wrapInMCTSNode(cstate_id=cstate_id,
+                                             previous_action=None,
+                                             cost_until_now=0,
+                                             is_goal=cstate.is_goal,
+                                             is_terminal=cstate.is_terminal,
+                                             # These next method calls are possible because TrainingMCTS is inside the service
+                                             as_network_input=self.problem_service.internal_to_network_input(cstate),
+                                             applicable_action_mask=self.problem_service.internal_get_applicable_action_mask(cstate),
+                                             hashed_state = cstate_hash)
+        self.state_id_to_node[self.curr_tree_root.state_id] = self.curr_tree_root
         # self.children.clear()
         self.N.clear()
         # self.Q.clear()
-        self.act_dist_per_node.clear()
+        # self.act_dist_per_node.clear()
+
+    def get_act_dist_from_mcts_node(self, node: MCTSNode):
+        if node.act_dist is None:
+            if node.as_network_input is None:
+                node.as_network_input = self.problem_service.exposed_to_network_input(*node.get_identifiers())
+            node.act_dist, value_tensor = to_local(self.network(node.as_network_input))
+            # value_tensor = to_local(value_tensor)
+            node.value = float(value_tensor.numpy().squeeze())
+        return tf.squeeze(node.act_dist)
 
     def run_search(self):
         """Run N simulations on current root and return π."""
@@ -90,7 +129,7 @@ class TrainingMCTS(MCTS):
         # Normalize visit counts
         if pi.sum() > 0:
             pi /= pi.sum()
-        else:
+        else: # Fallback to uniform distribution if stuff broke
             mask = self.get_applicable_action_mask(root)
             valid = np.where(mask)[0]
             if len(valid) > 0:
@@ -106,7 +145,7 @@ class TrainingMCTS(MCTS):
         next_node = parent.children[action_id]
         self.prune_children_except(parent, action_id)
         self.curr_tree_root = next_node
-        return self.curr_tree_root.state
+        return self.curr_tree_root.state_id, hash(self.curr_tree_root)
 
     def get_children_mask(self, act_dim=None, node=None):
         assert act_dim is not None, "Can't get a mask without a size!"
