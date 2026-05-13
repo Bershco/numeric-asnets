a3b8c851e0e34797ac6e083af836412d4be4e586
Bershco <hersco@post.bgu.ac.il>
2026-05-13 11:41:10 +0300

    lowered eval search limit for validation architectures

--- asnets/experiments_numeric/architecture/actprop_2l_comparison_mcts_valid.py
+++ asnets/experiments_numeric/architecture/actprop_2l_comparison_mcts_valid.py
@@ -1,3 +1,4 @@
 from .actprop_2l_comparison_mcts import *
 
 EXPLORATION_ALGORITHM = 'mcts_valid'
+EVAL_ROUND_TURN_LIMIT = 1000
--- asnets/experiments_numeric/architecture/actprop_2l_comparison_policy_valid.py
+++ asnets/experiments_numeric/architecture/actprop_2l_comparison_policy_valid.py
@@ -1,3 +1,4 @@
 from .actprop_2l_comparison_mcts import *
 
 EXPLORATION_ALGORITHM = 'policy_valid'
+EVAL_ROUND_TURN_LIMIT = 1000
