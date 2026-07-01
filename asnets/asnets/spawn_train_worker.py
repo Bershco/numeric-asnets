9fdfa91128c1a1cbe31f6ae2b8fc539d94ebff40
Bershco <hersco@post.bgu.ac.il>
2026-07-01 18:51:22 +0300

    typefix

--- asnets/asnets/spawn_train_worker.py
+++ asnets/asnets/spawn_train_worker.py
@@ -31,7 +31,7 @@ from asnets.spawn_context import LocalExploreContext
 from asnets.state_reprs import CanonicalState, get_init_cstate, sample_next_state
 from asnets.supervised import PlannerExtensions, planner_trace
 from asnets.utils.generator_utils import extract_domain_name_from_file, Domain, InstanceDifficulty
-from asnets.utils.py_utils import set_random_seeds, RandomPopContainer
+from asnets.utils.py_utils import set_random_seeds, RandomPopContainer,strip_parens
 from post_training.enhspwrapper import ENHSPEstimator, EstimatorMode
 from post_training.training_mcts import TrainingMCTS, get_est_v
 
@@ -418,7 +418,7 @@ def plan_to_trajectory(enhsp_config: str, pddl_files: list[str], act_ident_to_in
             act_ident = plan_res.plan[i]
             planner_exts_act_ind = ctx.planner_exts.act_ident_to_ind[act_ident]
             prev_state_pi[planner_exts_act_ind] = 1.0 #TODO: make sure this is the correct index to put there, problematic
-            print(f"act_ident={act_ident}, bound_act identifier={ctx.planner_exts.act_ident_to_mdpsim_act[act_ident].identifier}")
+            print(f"act_ident={act_ident}, bound_act identifier={ctx.planner_exts.act_ident_to_mdpsim_act[strip_parens(act_ident)].identifier}")
             applicable_action_id = ctx.planner_exts.problem_meta.act_unique_id_to_index(act_ident)
             # this should be the same order as acts_enabled in CanonicalState instances
             plan_states_pi.append(prev_state_pi)
