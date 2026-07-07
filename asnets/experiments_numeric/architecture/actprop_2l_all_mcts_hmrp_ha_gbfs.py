0cc5e64cc86b4b7526b1f365a424b70ba8a00dde
Bershco <hersco@post.bgu.ac.il>
2026-07-07 15:46:30 +0300

    returned the usage of numeric landmarks because it breaks original networks (parameter count not exaclty the same, falls apart)

--- asnets/experiments_numeric/architecture/actprop_2l_all_mcts_hmrp_ha_gbfs.py
+++ asnets/experiments_numeric/architecture/actprop_2l_all_mcts_hmrp_ha_gbfs.py
@@ -1,4 +1,3 @@
 from .actprop_2l_all_mcts import *
 
 ENHSP_CONFIG = 'hmrp-ha-gbfs'
-USE_NUMERIC_LANDMARKS = False #This arch currently used for rover, where numeric LMs are not helpful according to NuASNets
--- asnets/experiments_numeric/architecture/actprop_2l_comparison_mcts_hadd_astar.py
+++ asnets/experiments_numeric/architecture/actprop_2l_comparison_mcts_hadd_astar.py
@@ -1,4 +1,3 @@
 from .actprop_2l_comparison_mcts import *
 
 ENHSP_CONFIG = 'hadd-astar'
-USE_NUMERIC_LANDMARKS = False #This arch currently used for delivery, where numeric LMs are not helpful according to NuASNets
